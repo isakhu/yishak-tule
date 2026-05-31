8cea52230051867afcea815eb804263dd9a6fc39
Yishak Tule <yishakhak@gmail.com>
2026-05-31 18:09:19 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Projects from "@/components/Projects";
 import Experience from "@/components/Experience";
 import Services from "@/components/Services";
 import Blog from "@/components/Blog";
-import Testimonials from "@/components/Testimonials";
 import Resume from "@/components/Resume";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
@@ -23,7 +22,6 @@ export default function Home() {
         <Experience />
         <Services />
         <Blog />
-        <Testimonials />
         <Resume />
         <Contact />
       </main>
