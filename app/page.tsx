66c4e2312b05a6eaa41392b4b3af3e66970171bf
Yishak Tule <yishakhak@gmail.com>
2026-09-03 12:44:47 +0300

    style: remove services section from portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Projects from "@/components/Projects";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
-import Services from "@/components/Services";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
@@ -18,7 +17,6 @@ export default function Home() {
         <About />
         <Skills />
         <Experience />
-        <Services />
         <Contact />
       </main>
       <Footer />
